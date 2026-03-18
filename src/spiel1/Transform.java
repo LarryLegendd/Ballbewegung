502722450f1e589115f7270382cd345d7a0d0224
hammara0 <hammara0@CR2-31.sn-cjt.de>
2026-03-18 08:31:12 +0100

    transform gefixt

--- src/spiel1/Transform.java
+++ src/spiel1/Transform.java
@@ -8,14 +8,17 @@ public class Transform {
 	public Transform(Vector2 position, double rotation,Vector2 speed){
 		this.position =position;
 		this.rotation=rotation;
+		this.speed = speed;
 	}
 	
 	public Transform(Vector2 position, double rotation){
 		this.position =position;
 		this.rotation=rotation;
+		this.speed = new Vector2(0,0);
 	}
 	public Transform(Vector2 position){
 		this.position =position;
 		this.rotation=0;
+		this.speed = new Vector2(0,0);
 	}
 }
