bdb8844c78cf1114c05c1d3c2ca91d823192627e
LarryLegendd <markusphammer@gmail.com>
2026-04-27 14:08:28 +0200

    minicommit

---
+++ src/spiel1/EnemyArea.java
@@ -0,0 +1,14 @@
+package spiel1;
+
+import java.awt.*;
+
+public class EnemyArea {
+    Vector2 size;
+    Vector2 xpos;
+    Vector2 ypos;
+    public EnemyArea(Vector2 size, int xpos, int ypos){
+        this.size=size;
+    }
+
+
+}
